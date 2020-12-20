8e0794c292d2806d30fee99c1e0596922257ef51
Kamal Thakur <kamal.thakur@webdunia.net>
2020-12-20 14:16:46 +0530

    Initial project setup

---
+++ eRTC/eRTC.h
@@ -0,0 +1,18 @@
+//
+//  eRTC.h
+//  eRTC
+//
+//  Created by Kamal Thakur on 20/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for eRTC.
+FOUNDATION_EXPORT double eRTCVersionNumber;
+
+//! Project version string for eRTC.
+FOUNDATION_EXPORT const unsigned char eRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <eRTC/PublicHeader.h>
+
+
---
+++ eRTC/eRTCSDK.swift
@@ -0,0 +1,24 @@
+//
+//  eRTCSDK.swift
+//  eRTC
+//
+//  Created by Kamal Thakur on 20/12/20.
+//
+
+import Foundation
+
+open class eRTCSDK {
+    
+    public static let shared = eRTCSDK()
+    
+    private init(){ }
+    
+    public func setupConfigure(){
+        print("configuration success")
+    }
+}
+
+
+
+
+
